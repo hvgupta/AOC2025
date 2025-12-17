d466a481702f6f809732473c1979e8a683170299
hvgupta <hvgupta@outlook.in>
2025-12-17 14:33:10 +0800

    day 1 finished

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -1,8 +1,18 @@
 use crate::file_reader::FileLineIterator;
 
-pub fn run(part: u8)-> String {
+const LOCK_MOD: i32 = 100;
+
+fn mod_format(num: i32) -> i32 {
+    ((num % LOCK_MOD) + LOCK_MOD) % LOCK_MOD
+}
+
+pub fn run(part: u8) -> String {
     let rotations = FileLineIterator::new("src/day_01/input.txt").unwrap();
-    fn part_output(function: fn(&FileLineIterator) -> Result<u32, ()>, part: u8, rotations: &FileLineIterator) -> String {
+    fn part_output(
+        function: fn(&FileLineIterator) -> Result<u32, ()>,
+        part: u8,
+        rotations: &FileLineIterator,
+    ) -> String {
         match function(&rotations) {
             Ok(result) => format!("Part {}: {:?}", part, result),
             Err(_) => format!("Error in Part {}", part),
@@ -16,23 +26,44 @@ pub fn run(part: u8)-> String {
     };
 }
 
-fn run_part1(rotations: &FileLineIterator) -> Result<u32, ()>{
+fn run_part1(rotations: &FileLineIterator) -> Result<u32, ()> {
     let mut cur_num: i32 = 50;
     let mut zero_count: u32 = 0;
     for rotation in rotations.lines() {
         let (dir, num) = rotation.split_at(1);
         let num = num.parse::<i32>().unwrap();
-        cur_num = match dir {
+        cur_num = mod_format(match dir {
             "L" => cur_num - num,
             "R" => cur_num + num,
             _ => return Err(()),
-        } % 100;
+        });
 
         zero_count += (cur_num == 0) as u32;
     }
     Ok(zero_count)
 }
 
-fn run_part2(rotations: &FileLineIterator) -> Result<u32, ()>{
-    Ok(0)
+fn run_part2(rotations: &FileLineIterator) -> Result<u32, ()> {
+    let mut cur_num: i32 = 50;
+    let mut zero_found: u32 = 0;
+
+    for rotation in rotations.lines() {
+        let (dir, num) = rotation.split_at(1);
+        let num = num.parse::<i32>().unwrap();
+        println!("Current number: {}, rotation: {}", cur_num, rotation);
+        let new_cur_num = match dir {
+            "L" => cur_num - num,
+            "R" => cur_num + num,
+            _ => return Err(()),
+        };
+        let mut zero_increment = (new_cur_num.abs() / LOCK_MOD) as u32;
+        if new_cur_num <= 0 && cur_num > 0 {
+            zero_increment += 1;
+        }
+        println!("After rotation: {}, to be added: {}", new_cur_num, zero_increment);
+        zero_found += zero_increment;
+        cur_num = mod_format(new_cur_num);
+    }
+
+    Ok(zero_found)
 }
